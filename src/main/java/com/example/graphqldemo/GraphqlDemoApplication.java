233a98b8f6e33c35ce91211d105dfb9637718d61
msbaek <msbaek@ktown4u.com>
2022-07-18 13:56:12 +0900

    define graphql types
    - enable graphiql

--- src/main/java/com/example/graphqldemo/GraphqlDemoApplication.java
+++ src/main/java/com/example/graphqldemo/GraphqlDemoApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GraphqlDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GraphqlDemoApplication.class, args);
+        System.out.println("http://localhost:8080/graphiql");
     }
-
 }
