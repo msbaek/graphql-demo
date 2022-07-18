625e642c4e895b37bc60ebc71fec775a052a8479
msbaek <msbaek@ktown4u.com>
2022-07-18 14:35:56 +0900

    apply @BatchMapping

--- src/main/java/com/example/graphqldemo/GraphqlDemoApplication.java
+++ src/main/java/com/example/graphqldemo/GraphqlDemoApplication.java
@@ -3,17 +3,20 @@ package com.example.graphqldemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
 import java.time.Instant;
+
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.util.stream.Collectors.toMap;
 
 @SpringBootApplication
 public class GraphqlDemoApplication {
@@ -45,16 +48,20 @@ class CustomerController {
         return Mono.just(customer);
     }
 
-    @SchemaMapping(typeName = "Customer")
-    Mono<Account> account(Customer customer) {
-        System.out.println("account called for customer["+customer.id()+"]");
-        return Mono.just(new Account(customer.id(), "Account@"+ Instant.now()));
+    @BatchMapping
+    Map<Customer, Account> account(List<Customer> customers) {
+        System.out.println("account for " + customers.size() + " customers");
+        // account for 2 customers
+        return customers.stream()
+                .collect(toMap(c -> c, c -> new Account(c.id(), "Account@"+ Instant.now())));
     }
 
-    @SchemaMapping(typeName = "Customer")
-    Mono<Profile> profile(Customer customer) {
-        System.out.println("profile called for customer["+customer.id()+"]");
-        return Mono.just(new Profile(customer.id(), "Profile@"+ Instant.now()));
+
+    @BatchMapping
+    Map<Customer, Profile> profile(List<Customer> customers) {
+        System.out.println("profile for " + customers.size() + " customers");
+        return customers.stream()
+                .collect(toMap(c-> c, c -> new Profile(c.id(), "Profile@"+ Instant.now())));
     }
 }
 
